93525edc570f1ac73b0c99029023551131abb971
dev-chua <chua3940@gmail.com>
2022-07-11 11:52:04 +0800

    complete five controller function that bind to router route

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/chua-dev/go-bookstore-mysql/pkg/models"
+	"github.com/chua-dev/go-bookstore-mysql/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -32,7 +33,72 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
-	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Context-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	// Declare Book Struct Type
+	CreateBook := &models.Book{}
+
+	// Receive Json Params from Request
+	utils.ParseBody(r, CreateBook)
+
+	// Create Book and retrieve book struct object
+	b := CreateBook.CreateBook()
+
+	// Convert the book struct object to json for return
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	// Get ID from request Params
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing int")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	// Declare an empty book
+	var updateBook = &models.Book{}
+
+	// Parse the Request body params into readable format into empty book above
+	utils.ParseBody(r, updateBook)
+
+	// Vars get the ID params from URL
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing ID to int")
+	}
+
+	selectedBook, db := models.GetBookById(ID)
+	if updateBook.Name != "" {
+		selectedBook.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		selectedBook.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		selectedBook.Publication = updateBook.Publication
+	}
+
+	db.Save(&selectedBook)
+	res, _ := json.Marshal(selectedBook)
+	w.Header().Set("Context-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 
